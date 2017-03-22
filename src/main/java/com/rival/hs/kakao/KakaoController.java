91ed8aabe9e75e22dcfbeb636317036298a08464
Minwoo <pmw9027@naver.com>
2017-03-22 14:05:21 +0900

    Create KakaoAPI module

--- src/main/java/com/rival/hs/kakao/KakaoController.java
+++ src/main/java/com/rival/hs/kakao/KakaoController.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 /**
  * Created by Minwoo on 2017. 3. 14..
@@ -18,9 +16,13 @@ public class KakaoController {
     @Autowired
     private KakaoDao kakaoDao;
 
+    private KakaoAPI kakaoAPI = new KakaoAPI();
+
     @RequestMapping(value="/kakao", method = RequestMethod.POST)
     public String kakao(@RequestBody KakaoDo body, ModelMap map) {
 
+
+        System.out.println(kakaoAPI.send(body.getAccess_token()));
 
 
         kakaoDao.save(body);

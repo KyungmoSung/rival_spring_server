e9da2632c9ca4f602829361c617f6f9f6d3c124d
Sung Kyungmo <skm280@naver.com>
2017-03-23 16:10:52 +0900

    Add Getter and Setter in KakaoDo Add Annotation Field for get kakao_info data

--- src/main/java/com/rival/hs/kakao/KakaoDo.java
+++ src/main/java/com/rival/hs/kakao/KakaoDo.java
@@ -1,20 +1,85 @@
 package com.rival.hs.kakao;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 /**
  * Created by Minwoo on 2017. 3. 14..
  */
+
+@Document(collection = "USER_TB")
 public class KakaoDo {
 
-    private int id;
-    private String nick_name;
+    private String _id;
+    private String nickname;
     private String profile_image;
     private String thumbnail_image;
     private String status;
     private String date;
+    @Field("kakao_info")
+    private  String kakao;
 
-    public KakaoDo(int id, String nick_name, String profile_image, String thumbnail_image, String status, String date) {
-        this.id = id;
-        this.nick_name = nick_name;
+    public String getKakao() {
+        return kakao;
+    }
+
+    public void setKakao(String kakao) {
+        this.kakao = kakao;
+    }
+
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getProfile_image() {
+        return profile_image;
+    }
+
+    public void setProfile_image(String profile_image) {
+        this.profile_image = profile_image;
+    }
+
+    public String getThumbnail_image() {
+        return thumbnail_image;
+    }
+
+    public void setThumbnail_image(String thumbnail_image) {
+        this.thumbnail_image = thumbnail_image;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public KakaoDo(String _id, String nickname, String profile_image, String thumbnail_image, String status, String date) {
+        this._id = _id;
+        this.nickname = nickname;
         this.profile_image = profile_image;
         this.thumbnail_image = thumbnail_image;
         this.status = status;

908bbac4cac26816b2a26e359d704bc75b9ce466
Minwoo <pmw9027@naver.com>
2017-03-28 16:54:44 +0900

    war deploying setting

--- src/main/java/com/rival/hs/SpringMvcApplication.java
+++ src/main/java/com/rival/hs/SpringMvcApplication.java
@@ -1,39 +1,17 @@
 package com.rival.hs;
 
-import com.rival.hs.game.GameDo;
-import com.rival.hs.game.GameMongoRepository;
-import com.rival.hs.user.UserDo;
-import com.rival.hs.user.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 
+@Configuration
 @ComponentScan
 @SpringBootApplication
+public class SpringMvcApplication {
 
-public class SpringMvcApplication  implements CommandLineRunner {
-
-	@Autowired
-	GameMongoRepository gameMongoRepository;
-	UserRepository userRepository;
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringMvcApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-
-		//gameMongoRepository.save(new GameDo("축구","서울","3","축구할사람",5,"올림픽경기장","7","2017.3.13 17:00"));
-
-		//userRepository.save(new UserDo("tkd","park",25));
-
-	}
-
 }
-
-

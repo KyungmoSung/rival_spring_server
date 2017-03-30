9ff89ba82d18e103c6c8e96948140c625a4ef361
Minwoo <pmw9027@naver.com>
2017-03-30 18:29:57 +0900

    update game module

--- src/main/java/com/rival/hs/SpringMvcApplication.java
+++ src/main/java/com/rival/hs/SpringMvcApplication.java
@@ -7,8 +7,18 @@ import com.rival.hs.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jersey.JerseyProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+
+import java.nio.charset.Charset;
 
 @Configuration
 @ComponentScan
@@ -25,5 +35,10 @@ public class SpringMvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcApplication.class, args);
+	}
+
+	@Bean
+	public HttpMessageConverter<String> responseBodyConverter() {
+		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
 	}
 }
--- src/main/java/com/rival/hs/game/GameController.java
+++ src/main/java/com/rival/hs/game/GameController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URLDecoder;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -32,46 +33,13 @@ public class GameController {
     // 축구, 풋볼 게시판 가져오기
     @RequestMapping(value="/board", method = RequestMethod.GET)
     public String SoccerBoard(Model model, @RequestParam(value="type", required = false) String type){
-        String soccer = "축구";
-        String baseball = "야구";
-        String basketball = "농구";
-        String volleyfootball = "족구";
-        String billiards = "당구";
-        String bowing = "볼링";
+
+        System.out.println(type);
         List<GameDo> board = gameMongoRepository.findByType(type);
-      /*  for (GameDo types: board) {
-            //축구
-            if(soccer.equals(type)) {
-                model.addAttribute("board",board);
-                return "board";
-            }
-            //야구
-            else if(baseball.equals(type)){
-                model.addAttribute("board",board);
-                return "board";
-            }
-            //농구
-            else if(basketball.equals(type)){
-                model.addAttribute("board",board);
-                return "board";
-            }
-            //족구
-            else if(volleyfootball.equals(type)){
-                model.addAttribute("board",board);
-                return "board";
-            }
-            //당구
-            else if(billiards.equals(type)){
-                model.addAttribute("board",board);
-                return "board";
-            }
-            //볼링
-            else if(bowing.equals(type)){
-                model.addAttribute("board",board);
-                return "board";
-            }
-        }*/
+
         model.addAttribute("board",board);
+        model.addAttribute("title",type);
+
         return "board";
     }
 
